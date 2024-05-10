95d276c4087c9733811c22c1fc14bbba1eb48907
JensenMPillay <jmooroongapillay@gmail.com>
2024-05-10 22:49:03 +0200

    [Build] Update docker-entrypoint commands

--- docker-entrypoint-php-worker.sh
+++ docker-entrypoint-php-worker.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+if [ "$(find ./migrations -iname '*.php' -print -quit)" ]; then
+    php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing
+fi
+
+if [ "$(find ./src/DataFixtures -iname '*.php' -print -quit)" ] && [ "$(php bin/console dbal:run-sql "SELECT count(*) FROM module" | grep -oP '\d+')" -eq 0 ]; then
+    php bin/console doctrine:fixtures:load --append
+fi
+
 if [ "$( find ./src/Scheduler -type f -iname '*.php' -print -quit )" ]; then
     exec php bin/console messenger:consume -v scheduler_default
 fi
